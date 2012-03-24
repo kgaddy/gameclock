623d21548ea2b18ae88696eb786e979bf209eeea
Kevin Gaddy <Kevin.Gaddy@gmail.com>
2012-03-24 13:03:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# gameclock.js 
+
+## A javascript library creating timer/game clocks for games
+
+
+
+gameclock.js is a javascript library that can be used to create a timer/clock for games:
+
+
+## Documentation
+
+
+A step by step guide on how to use gameclock.js:
+
+
+## Demo's
---
+++ js/gameclock.js
@@ -0,0 +1,86 @@
+var Clock = function(config) {
+		console.dir(config);
+		var clock = this;
+		var time = config.time,
+			remainingTime = time,
+			qClock, minutes = 0,
+			resetAfterTimeout = true,
+			stopAferTimeout = true,
+			seconds = 0,
+			timeoutCallBack,
+			resume = true;
+		assignUSerSettings();
+		displayClock();
+
+
+		function assignUSerSettings() {
+			if (config.resetAfterTimeout != null) {
+				resetAfterTimeout = config.resetAfterTimeout;
+			}
+
+			if (config.stopAferTimeout != null) {
+				stopAferTimeout = config.stopAferTimeout;
+			}
+
+			if(config.timeoutCallBack!=null){
+				timeoutCallBack = config.timeoutCallBack;
+			}
+
+		}
+
+		function calculateTime() {
+			if (resume === true) {
+				if (remainingTime <= 0) {
+					if (resetAfterTimeout === true) {
+						clock.ResetClock();
+						resume = true;
+					}
+					if (stopAferTimeout === true) {
+						clock.StopClock();
+						resume = false;
+					}
+
+					if(timeoutCallBack!=null){
+						timeoutCallBack();
+					}
+				}
+				if (resume === true) {
+					remainingTime = remainingTime - 1000;
+					displayClock();
+				}
+			}
+		}
+		//The actual calculation that converts the milliseconds to a mm:ss  and assigns to DOM
+
+		function displayClock() {
+			minutes = Math.floor(remainingTime / 1000 / 60);
+			seconds = Math.floor(remainingTime / 1000) % 60;
+			var displaySec = seconds;
+			if (seconds < 10) {
+				displaySec = '0' + seconds;
+			}
+			if (config.displayEL != null) {
+				document.getElementById(config.displayEL).innerHTML = minutes + ":" + displaySec;
+			}
+
+		}
+		//Starts the clock
+		this.StartClock = function() {
+			resume=true;
+			qClock = setInterval(function() {
+				calculateTime();
+			}, 1000);
+		}
+		//stops the clock
+		this.StopClock = function() {
+			clearInterval(qClock)
+		}
+		//resets the clock
+		this.ResetClock = function() {
+			time = config.time;
+			remainingTime = time;
+			qClock, minutes = 0;
+			seconds = 0;
+			displayClock();
+		}
+	}
